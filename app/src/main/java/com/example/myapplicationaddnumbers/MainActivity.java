39ed434518a086d6a3130448cdc00ed98dc2e8dc
joting <esjoting@gmail.com>
2023-02-22 11:03:26 +0800

    ADD_SUB Two Numbers

--- app/src/main/java/com/example/myapplicationaddnumbers/MainActivity.java
+++ app/src/main/java/com/example/myapplicationaddnumbers/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplicationaddnumbers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,9 +12,17 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private EditText num1;
     private EditText num2;
+
     private Button add;
+
     private TextView result;
 
+    private Button sub;
+
+    public MainActivity() {
+    }
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         num2 = (EditText)findViewById(R.id.txtNum2);
         add = (Button)findViewById(R.id.btnAdd);
         result = (TextView)findViewById(R.id.txtResult);
+        sub = (Button) findViewById(R.id.btnSub);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +45,20 @@ public class MainActivity extends AppCompatActivity {
 
                 result.setText("Answer is "+sum);
 
+            }
+        });
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get Value form Activity
+                int myNumber1 = Integer.parseInt(num1.getText().toString());
+                int myNumber2 = Integer.parseInt(num2.getText().toString());
+
+                //add operation
+                int ans = myNumber1 - myNumber2;
+
+                result.setText("Answer is "+ans);
             }
         });
 
