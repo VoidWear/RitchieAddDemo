e9984c88dc5e2586b5ebe22e23f90310e9b28690
joting <esjoting@gmail.com>
2023-02-22 10:14:38 +0800

    Add Two Numbers

---
+++ app/src/main/java/com/example/myapplicationaddnumbers/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.myapplicationaddnumbers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText num1;
+    private EditText num2;
+    private Button add;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        num1 = (EditText)findViewById(R.id.txtNum1);
+        num2 = (EditText)findViewById(R.id.txtNum2);
+        add = (Button)findViewById(R.id.btnAdd);
+        result = (TextView)findViewById(R.id.txtResult);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get Value form Activity
+                int myNumber1 = Integer.parseInt(num1.getText().toString());
+                int myNumber2 = Integer.parseInt(num2.getText().toString());
+
+                //add operation
+                int sum = myNumber1 + myNumber2;
+
+                result.setText("Answer is "+sum);
+
+            }
+        });
+
+    }
+}
